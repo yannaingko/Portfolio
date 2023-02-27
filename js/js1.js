5d2f11b273dde9f67d8b10390938bf7697b50b46
yannaingko <30681530+yannaingko@users.noreply.github.com>
2023-02-27 11:38:39 +0630

    Update js1.js

--- js/js1.js
+++ js/js1.js
@@ -153,3 +153,33 @@ wordflick1();
 $(document).ready(function () {
 wordflick2();
 });
+// for scroll android
+view.setOnTouchListener(new OnTouchListener() {
+
+    private long startClickTime;
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            startClickTime = System.currentTimeMillis();
+
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+
+            if (System.currentTimeMillis() - startClickTime < ViewConfiguration.getTapTimeout()) {
+
+                // Touch was a simple tap. Do whatever.
+
+            } else {
+
+                // Touch was a not a simple tap.
+
+            }
+
+        }
+
+        return true;
+    }
+
+});
